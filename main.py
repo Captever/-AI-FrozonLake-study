6a39879aedca4c2a811bdc833bca9707b0645881
Captever-Desktop <captever6@gmail.com>
2025-05-14 23:57:59 +0900

    Refactor load_image function to handle alpha channel and optional scaling

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ class Main:
     def __init__(self):
         pygame.init()
 
-        self.map_size = 4 if GAME_MODE == 1 else 8
-        
-        tile_size = SCREEN_SIZE // self.map_size
+        map_size = 4 if GAME_MODE == 1 else 8
+        tile_size = SCREEN_SIZE // map_size
+        object_size = tile_size * 0.8
 
         pygame.display.set_caption("Frozen Lake")
         self.screen = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
@@ -22,24 +22,33 @@ class Main:
         self.clock = pygame.time.Clock()
 
         self.assets = {
-            'frozen': load_image('tiles/frozen.png'),
-            'hole': load_image('tiles/hole.png'),
-            'player_back': load_image('objects/player_back.png'),
-            'player_front': load_image('objects/player_front.png'),
-            'player_left': load_image('objects/player_left.png'),
-            'player_right': load_image('objects/player_right.png'),
-            'branch_start': load_image('objects/branch_start.png'),
-            'branch_goal': load_image('objects/branch_goal.png'),
+            'frozen': load_image('tiles/frozen.png', tile_size),
+            'hole': load_image('tiles/hole.png', tile_size),
+            'player_back': load_image('objects/player_back.png', object_size),
+            'player_front': load_image('objects/player_front.png', object_size),
+            'player_left': load_image('objects/player_left.png', object_size),
+            'player_right': load_image('objects/player_right.png', object_size),
+            'branch_start': load_image('objects/branch_start.png', object_size),
+            'branch_goal': load_image('objects/branch_goal.png', object_size),
         }
         
         self.player = Player()
 
-        self.tilemap = Tilemap(self, map_size=self.map_size, tile_size=tile_size)
+        self.tilemap = Tilemap(self, map_size=map_size, tile_size=tile_size)
+
+        self.start_loc = 0
+        self.goal_loc = map_size ** 2 - 1
+
+        self.tilemap.update(self.start_loc, 0)
+        self.tilemap.update(self.goal_loc, 0)
 
     def run(self):
-        self.tilemap.update(0, 0)
-        self.tilemap.update(self.map_size ** 2 - 1, 0)
-        
+        for i in range(1, 34):
+            self.tilemap.update(i, 0)
+        self.tilemap.update(10, 1)
+        self.tilemap.update(3, 1)
+        self.tilemap.update(30, 1)
+
         while True:
 
             for event in pygame.event.get():
--- scripts/utils.py
+++ scripts/utils.py
@@ -4,9 +4,17 @@ import pygame
 
 BASE_IMG_PATH = "resources/"
 
-def load_image(path):
-    img = pygame.image.load(BASE_IMG_PATH + path).convert()
+def load_image(path, size=None):
+    img = pygame.image.load(BASE_IMG_PATH + path).convert_alpha()  # handle alpha channel
     img.set_colorkey((0, 0, 0)) # black to transparency
+
+    if size:  # apply scaling only when `size` is not `None`
+        width, height = img.get_size()
+        if width > height:
+            factor = size / float(width)
+        else:
+            factor = size / float(height)
+        img = pygame.transform.scale_by(img, factor)
 
     return img
 
