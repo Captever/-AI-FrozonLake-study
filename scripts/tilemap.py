2b7a751ad36f58c3e3e22bc91476c4fd95d17132
Captever-Desktop <captever6@gmail.com>
2025-05-14 15:43:04 +0900

    Refactor Tilemap class to streamline initialization and rendering logic

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -2,31 +2,25 @@ import json
 
 import pygame
 
-PHYSICS_TILES = {'frozen', 'hole'}
+TILE_LIST = ['frozen', 'hole']
 
 class Tilemap:
-    def __init__(self, game, tile_size=16):
+    def __init__(self, game, map_size, tile_size):
         self.game = game
+        self.map_size = map_size
         self.tile_size = tile_size
-        self.tilemap = {}
-        self.offgrid_tiles = []
+        self.tiles = [-1 for _ in range(map_size ** 2)]
 
-    def tiles_around(self, pos):
-        tiles = []
+    def update(self, loc: int, tile_type: int):
+        self.tiles[loc] = tile_type
+        
+    def render(self, surf: pygame.Surface, offset=(0, 0)):
+        for loc, tile_type in enumerate(self.tiles):
+            if tile_type == -1:  # if there is no tile
+                continue
 
-        # '//' and '/' produce different results, so '//' must be used
-        tile_loc = (int(pos[0] // self.tile_size), int(pos[1] // self.tile_size))
-        
-        return tiles
-        
-    def render(self, surf, offset=(0, 0)):
-        for tile in self.offgrid_tiles:
-            surf.blit(self.game.assets[tile['type']], (tile['pos'][0] - offset[0], tile['pos'][1] - offset[1]))
-            
-        for x in range(offset[0] // self.tile_size, (offset[0] + surf.get_width()) // self.tile_size + 1):
-            for y in range(offset[1] // self.tile_size, (offset[1] + surf.get_height()) // self.tile_size + 1):
-                loc = str(x) + ';' + str(y)
-                if loc in self.tilemap:
-                    tile = self.tilemap[loc]
-                    surf.blit(self.game.assets[tile['type']], 
-                              (tile['pos'][0] * self.tile_size - offset[0], tile['pos'][1] * self.tile_size - offset[1]))
+            y, x = divmod(loc, self.map_size)
+            tile_pos = (x * self.tile_size, y * self.tile_size)
+            curr_tile = TILE_LIST[tile_type]
+
+            surf.blit(self.game.assets[curr_tile], [tile_pos[i] + offset[i] for i in range(2)])
